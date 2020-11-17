42f036e146b3e5599c95fffc9f398bab1e4bd13f
Manuel Nunez <03.manu@gmail.com>
2020-11-17 02:05:50 +0100

    Bug Fix product search url generator

--- app/Http/Controllers/ProductSearchController.php
+++ app/Http/Controllers/ProductSearchController.php
@@ -15,11 +15,12 @@ class ProductSearchController extends BaseController
         //     ->registerModel(Product::class, ['name', 'price'])
         //     ->search($searchTerm);
         $product = Product::where('name', 'LIKE', "%{$searchTerm}%")->first();
-        $category = $product->itemCategoryProduct->categoryItem->categoryItemCategory->category;
+        $itemCategory = $product->itemCategoryProduct->categoryItem;
+        $category = $itemCategory->categoryItemCategory->category;
         $section = $category->categorySection->section;
 
          
-        $url = route('inicio', "/section/".$section->name."/category/".$category->name."/item/".$product->name);
+        $url = route('inicio', "/section/".$section->section_id."/category/".$category->category_id."/item/".$itemCategory->item_category_id);
         return $this->sendResponse($url, "Success!");
     }
 }
