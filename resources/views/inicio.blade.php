7adda3750a066c8ba6a3cbf84870906740fc938a
Manuel Nunez <03.manu@gmail.com>
2020-11-17 13:51:19 +0100

    Reordering script in inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -231,20 +231,11 @@
                  $('.collapse.in').toggleClass('in');
                  $('a[aria-expanded=true]').attr('aria-expanded', 'false');
              });
+         });
+     </script>
 
-             $('.first-button').on('click', function() {
-
-                 $('.animated-icon1').toggleClass('open');
-             });
-             $('.second-button').on('click', function() {
-
-                 $('.animated-icon2').toggleClass('open');
-             });
-             $('.third-button').on('click', function() {
-
-                 $('.animated-icon3').toggleClass('open');
-             });
-
+     <script>
+         $(document).ready(function() {
              //On pressing a key on "Search box" in "search.php" file. This function will be called.
              $("#myInput").keyup(function() {
                  //Assigning search box value to javascript variable named as "name".
@@ -288,6 +279,24 @@
              console.log(html)
              return '<ul>' + html + '</ul>';
          }
+     </script>
+
+     <script>
+         $(document).ready(function() {
+
+             $('.first-button').on('click', function() {
+
+                 $('.animated-icon1').toggleClass('open');
+             });
+             $('.second-button').on('click', function() {
+
+                 $('.animated-icon2').toggleClass('open');
+             });
+             $('.third-button').on('click', function() {
+
+                 $('.animated-icon3').toggleClass('open');
+             });
+         });
      </script>
  </body>
 
